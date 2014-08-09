34841f9533041c9579c592908d4ebacc93adfcf8
Gennady Denisov <denisovgena@gmail.com>
2014-08-09 15:23:59 +0400

    Fix update script. Get rid of yaml manipulation.

--- update.py
+++ update.py
@@ -11,22 +11,7 @@ __author__ = 'Gennady Denisov <denisovgena@gmail.com>'
 
 import sys
 import json
-import yaml
 import datetime
-
-
-def appcfg():
-    """
-    Update app.yaml if necessary
-    """
-    # Update DEBUG state to False
-    with open('app.yaml', 'r') as f:
-        app_cfg = yaml.safe_load(f)
-    app_cfg['env_variables']['DEBUG'] = 'False'
-    app_cfg['version'] = 'test'
-    stream = yaml.dump(app_cfg, default_flow_style=False)
-    with open('app.yaml', 'w') as f:
-        f.write(stream)
 
 
 def update():
