909111b0d4fa2da85ae64a854b1322b9c7e99507
Gennady Denisov <denisovgena@gmail.com>
2014-08-24 10:11:08 +0400

    Update script populates commit hash. Update angular js to 1.2.23

--- update.py
+++ update.py
@@ -18,9 +18,11 @@ def update():
     with open('data/versions.json', 'r') as f:
         versions = json.load(f)
     versions['libs'] = [
-        {'lib': 'Google App Engine', 'version': '1.9.8'},
+        {'lib': 'Google App Engine', 'version': '1.9.9'},
         {'lib': 'Python', 'version': sys.version.split(' ')[0]}]
     versions['last_update'] = datetime.datetime.now().isoformat()
+    git_hash = sys.argv[1]
+    versions['hash'] = git_hash
     with open('data/versions.json', 'w') as f:
         json.dump(versions, f, indent=4)
 
